78202ceaa8f530e96faeebd7702bb713c4838d21
Mira <athes01@gmail.com>
2025-07-03 18:47:45 +0200

    generate link

--- src/lib/Code.svelte
+++ src/lib/Code.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
+	import { onMount, type Snippet } from 'svelte';
+	import * as lzstring from 'lz-string';
 
 	interface Props {
 		children?: Snippet;
 	}
 
 	const { children }: Props = $props();
+
+	let code_elem: HTMLDivElement;
+
+	let link = $state('');
+
+	function generatePlaygroundURL(code: string): string {
+		const compressed = lzstring.compressToEncodedURIComponent(code);
+		return `https://www.typescriptlang.org/play?#code/${compressed}`;
+	}
+
+	function update_playground_link() {
+		const code = code_elem.innerText;
+		link = generatePlaygroundURL(code);
+	}
+
+	onMount(() => {
+		update_playground_link();
+	});
 </script>
 
-Code block:
+<div bind:this={code_elem}>
+	{@render children?.()}
+</div>
 
-{@render children?.()}
+<a href={link} target="_blank">Link to TS Playground</a>
