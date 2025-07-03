c00c7ed28a50f456f5ed3e18632c4c2c4c608ec0
Mira <athes01@gmail.com>
2025-07-03 18:37:36 +0200

    test

---
+++ src/lib/Code.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		children?: Snippet;
+	}
+
+	const { children }: Props = $props();
+</script>
+
+Code block:
+
+{@render children?.()}
